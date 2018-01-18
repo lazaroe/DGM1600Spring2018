e7e8cb9bb0141f39e676c8f4238a877b37288903
Unknown <pedicho@gmail.com>
2018-01-18 11:15:15 -0700

    Worked on animal class, added click on noise

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+    //this declares what start will do
+	void Start () {
+        print("my animal is eating.");
+        print("now my animal is sleeping.");
+        print("I love my dog");
+	}
+
+    void OnMouseDown()
+    {
+        print("Woof");
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
