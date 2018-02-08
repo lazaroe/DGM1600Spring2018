0800ca8941ab5dca1f679657efbb927d97f1d375
Unknown <pedicho@gmail.com>
2018-02-08 11:22:08 -0700

    Math Operators

---
+++ FirstGame/Assets/MathOperators.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathOperators : MonoBehaviour {
+
+	//public int Score = 0;
+	//public int PowerUp = 10;
+	//public int C = 10;
+
+
+	// Use this for initialization
+	void OnTriggerEnter () {
+
+		//Score += PowerUp
+	}
+}
+
+//The job of a math operator is in essance to do Math
+//int is whole number. float is decimals
+//A++ increments by 1
+//A-- Decrements by 1
+//C is assigned to A & B therefore it becomes 20. then if placed right below C being 20 can be added to by putting +=
--- FirstGame/Assets/PowerUp.cs
+++ FirstGame/Assets/PowerUp.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 [CreateAssetMenu]
 public class PowerUp : ScriptableObject {
-	public int PowerLevel = 100;
+	public int PowerLevel = 10;
+	public Player CurrentPlayer; 
+	public void RunPowerUp ()
+	{
+		//Adds Value
+		CurrentPlayer.Score += PowerLevel;
+	}
 	public string Name = "Health";
 
 }
 
 //Scriptable objects downsize the mass of the file, Monobehaviour is a bigger box. Mono can be used as scriptable objects on unity.
 // you place ; only if you are going to assign it to do something
+//Debug means find and Log menas report
+// CreateAssetMenu will enable you to make it an asset that you can utilize and maniupalte differently
