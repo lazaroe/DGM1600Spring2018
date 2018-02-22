a4795df0b83dc2ad0b0467d4eeaa337d2e8431c9
Unknown <pedicho@gmail.com>
2018-02-22 13:19:06 -0700

    Void Functions

---
+++ FirstGame/Assets/Scripts/MoreWork/VoidFunctions.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariablesAndFunctions : MonoBehaviour
+{   
+    int myInt = 5;
+    
+    
+    void Start ()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log (myInt);
+    }
+    
+    
+    int MultiplyByTwo (int number)
+    {
+        int ret;
+        ret = number * 2;
+        return ret;
+    }
+}
+
+//Void specefies functions that return no value.
+//Void take no parameters
+//ret stands for return. 
+//
