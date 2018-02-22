812ef0cc2ea05685d935fde53d93a4de911980e2
Unknown <pedicho@gmail.com>
2018-02-22 13:29:29 -0700

    Parameters and Functions

---
+++ FirstGame/Assets/Scripts/MoreWork/parameters.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parameters : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+}
+//Each Parameter needs to have descriptive names.
+//a method can recieve data by specifying a parameter, and then can send it back by putting a return function.
+//parameters read and record what is listed by name.
