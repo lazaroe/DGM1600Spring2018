74258b0c1ca9cf5dd0adce2f3d92e10b36339fa7
Unknown <pedicho@gmail.com>
2018-03-08 13:20:43 -0700

    Scope and Access modifiers part 2

---
+++ FirstGame/Assets/Scope and AccessModifiers.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAndAccessModifiers : MonoBehaviour
+{
+    public int alpha = 5;
+    
+    
+    private int beta = 0;
+    private int gamma = 5;
+    
+    
+    private AnotherClass myOtherClass;
+    
+    
+    void Start ()
+    {
+        alpha = 29;
+        
+        myOtherClass = new AnotherClass();
+        myOtherClass.FruitMachine(alpha, myOtherClass.apples);
+    }
+    
+    
+    void Example (int pens, int crayons)
+    {
+        int answer;
+        answer = pens * crayons * alpha;
+        Debug.Log(answer);
+    }
+    
+    
+    void Update ()
+    {
+        Debug.Log("Alpha is set to: " + alpha);
+    }
+}
+public class AnotherClass
+{
+    public int apples;
+    public int bananas;
+    
+    
+    private int stapler;
+    private int sellotape;
+    
+    
+    public void FruitMachine (int a, int b)
+    {
+        int answer;
+        answer = a + b;
+        Debug.Log("Fruit total: " + answer);
+    }
+    
+    
+    private void OfficeSort (int a, int b)
+    {
+        int answer;
+        answer = a + b;
+        Debug.Log("Office Supplies total: " + answer);
+    }
+}
+//the scope of the variable is the area in which it can be used in
+//Everything within the clause can be set local to that class
+//if you twick the value of the inspectors in the editor. you can edit it only withing the calss if its private.
+//
