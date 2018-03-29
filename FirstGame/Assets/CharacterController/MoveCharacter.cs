4f0ad5e1c3324f98b67b4c97d9a65b5b00e4ceb1
Unknown <pedicho@gmail.com>
2018-03-29 11:17:21 -0600

    Google search scriptable objects:firefly

--- FirstGame/Assets/CharacterController/MoveCharacter.cs
+++ FirstGame/Assets/CharacterController/MoveCharacter.cs
@@ -5,31 +5,15 @@ using UnityEngine.UI;
 
 public class MoveCharacter : MonoBehaviour {
 
-	private CharacterController controller;
-    private Vector3 moveDirection = Vector3.zero;
-
     public Movement Movement;
-
+	private CharacterController controller;
 	private void Start()
 	{
 		controller = GetComponent<CharacterController>();
 	}
     void Update() {
-      
-        if (controller.isGrounded) {
-			moveDirection.x = Input.GetAxis("Horizontal");
-			moveDirection.z = 0;
-			moveDirection.y = Input.GetAxis("Vertical");
-			
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= Movement.speed;
-
-            if (Input.GetButton("Jump"))
-                moveDirection.y = Movement.jumpSpeed;
-            
-        }
-        moveDirection.y -= Movement.gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+      Movement.Move(controller, transform);
+        
     }
 }
 
--- FirstGame/Assets/CharacterController/Movement.cs
+++ FirstGame/Assets/CharacterController/Movement.cs
@@ -2,12 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+
 [CreateAssetMenu]
 public class Movement : ScriptableObject {
-	public float speed = 6.0F;
+
+    public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
 
+    private Vector3 moveDirection = Vector3.zero;
 
+    public void Move(CharacterController controller, Transform transform)
+    {
+        if (controller.isGrounded) {
+			moveDirection.x = Input.GetAxis("Horizontal");
+			moveDirection.z = Input.GetAxis("Vertical");
+			moveDirection.y = 0;
+			
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+            
+            
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
 
 }
