f600ea94695e7e17a0f9ce47e9d18c350e005b1c
Unknown <pedicho@gmail.com>
2018-04-07 20:16:38 -0600

    more changes

--- FirstGame/Assets/MyGame/Scripts/PlayerMovement.cs
+++ FirstGame/Assets/MyGame/Scripts/PlayerMovement.cs
@@ -3,14 +3,30 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-
-    public float speed;
-    public Text countText;
+    
+       public Text countText;
     public Text winText;
 
     private Rigidbody rb;
     private int count;
 
+   public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    void Update() {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded) {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+            
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
