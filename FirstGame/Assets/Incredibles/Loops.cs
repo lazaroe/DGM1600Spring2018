a9f395f61ee56327a2ac79390dbd5515ee6bdf4e
Unknown <pedicho@gmail.com>
2018-04-07 19:54:31 -0600

    My Actual game progress

---
+++ FirstGame/Assets/Incredibles/Loops.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//public class Loops : MonoBehaviour {
+	//public string[] Keys;
+
+	// Use this for initialization
+	//void Start () 
+	//{
+			//Run();
+
+			//private void Update()
+		//	{
+				//if (Input.GetKeyDown(KeyCode.Space))
+			//	{
+				//	Run();
+			//	}
+		//	}
+	//void Run()
+	//	{
+		//for (int i = 0; i < Keys.Length; i++)
+	//	{
+		///	if (Keys[i] == "Add Ammo")
+		//	{
+			//	print (Keys[i]);
+		//	}
+	//	}
+	//}
+//}
+//}
+//for (); loops are just a pattern. It means that the work will continue to loop
+//for loops are loops that can be specified. You declare I=0 
+//scriptable objects can be saved.
+//has to be set to 0 because that is where is starts.
+//sometimes you can't use foreach loop. You have to use a for loop instead.
+//i < Keys.Length; is an If statement. just that it's programed. and the "then" is i++ which means add one to the list.
+//Size is the same word in unity as length. Collection is name, which is Key. 
+//you would loop if you want to look for data
+//i is shortcut for index. 
+//For Void statements the first is to call it. and the second is to make it work.
---
+++ FirstGame/Assets/MyGame/Scripts/PlayerCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	public float zoomSpeed = 4f;
+	public float minZoom = 5f;
+	public float maxZoom = 15f;
+
+	public float yawSpeed = 100f;
+	private float currentZoom = 10f;
+	private float currentYaw = 0;
+
+    void Start ()
+    {
+        offset = transform.position - player.transform.position;
+    }
+    
+    void LateUpdate ()
+    {
+        transform.position = player.transform.position + offset;
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+		currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+		currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+    }
+}
