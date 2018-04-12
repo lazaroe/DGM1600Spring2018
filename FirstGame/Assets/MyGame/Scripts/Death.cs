433161097e2c25608a32ba140cf63d0bd017053c
Unknown <pedicho@gmail.com>
2018-04-12 14:04:01 -0600

    More changes

--- FirstGame/Assets/MyGame/Scripts/Death.cs
+++ FirstGame/Assets/MyGame/Scripts/Death.cs
@@ -15,10 +15,7 @@ void OnTriggerEnter (Collider other) {
 	if (other.CompareTag("Player"))
 		{
 			Destroy (other.gameObject);
-		}
-	if (other.CompareTag("Ground"))
-		{
-		Destroy (gameObject);
+			Destroy (gameObject);
 		}
 	}
 }
