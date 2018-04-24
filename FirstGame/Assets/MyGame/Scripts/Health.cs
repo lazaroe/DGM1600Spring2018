5f89b26925c859f2337924158868556ff1288b51
Unknown <pedicho@gmail.com>
2018-04-23 20:00:13 -0600

    more adjustments

--- FirstGame/Assets/MyGame/Scripts/Health.cs
+++ FirstGame/Assets/MyGame/Scripts/Health.cs
@@ -7,6 +7,11 @@ public class Health : MonoBehaviour {
     public float PowerLevel = 1.0F;
     public Image HealthBar;
 
+    public float AddPowerLevel()
+    {
+        return HealthBar.fillAmount += PowerLevel;
+    }
+
     public void OnTriggerEnter(Collider obj)
     {
         
