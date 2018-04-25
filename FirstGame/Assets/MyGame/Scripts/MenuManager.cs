f5ed00567304192ba92f1e437862a02d755538c6
Unknown <pedicho@gmail.com>
2018-04-24 21:27:18 -0600

    finished the end game button. Twiked up some sounds

--- FirstGame/Assets/MyGame/Scripts/MenuManager.cs
+++ FirstGame/Assets/MyGame/Scripts/MenuManager.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
 
+	AudioManager audioManager;
+
 	public void QuitGame()
 	{
+		Debug.Log("WE QUIT THE GAME!");
 		Application.Quit();
 	}
+
 }
+//with the help of tutoriol
