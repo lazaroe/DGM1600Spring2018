f3201c943cdaa2c73bda92cb975d5226ec7aa3ff
Unknown <pedicho@gmail.com>
2018-04-24 20:14:25 -0600

    Made Sound fiiles

--- FirstGame/Assets/MyGame/Scripts/AudioManager.cs
+++ FirstGame/Assets/MyGame/Scripts/AudioManager.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour{
+    public AudioClip sound;
+    int SoundLoudness = 1;
 
+     void Start()
+     {
+         for(int l = 0; l < SoundLoudness;l++)
+        {
+            
+        }
+     }
 }
 
