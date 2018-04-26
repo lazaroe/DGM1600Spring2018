b13908c7df434dfdb660e328a39224f46f3e0c68
Unknown <pedicho@gmail.com>
2018-04-26 12:39:51 -0600

    some type of classes

--- FirstGame/Assets/MyGame/Scripts/SpawnEnemy.cs
+++ FirstGame/Assets/MyGame/Scripts/SpawnEnemy.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour {
+
+    public class enemy1{
+        public int enemy = 1;
+        public enemy1 (int enm)
+        {
+            enemy = enm;
+        }
+    }
      public float Timer = 2;
      public GameObject clone;
      int Enemies = 1;
@@ -14,7 +22,7 @@ public class SpawnEnemy : MonoBehaviour {
      {
          for(int l = 0; l < Enemies;l++)
         {
-            print ("Cloning Enemy" + 1); 
+            print ("enm" + 1); 
         }
      }
      void Update()
