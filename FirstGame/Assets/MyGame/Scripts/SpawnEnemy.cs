93f756776a309fceed085675729e59fd7877a57d
Unknown <pedicho@gmail.com>
2018-04-28 17:00:00 -0600

    final changes

--- FirstGame/Assets/MyGame/Scripts/SpawnEnemy.cs
+++ FirstGame/Assets/MyGame/Scripts/SpawnEnemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour {
-    
+
     public class enemy1{
         public float enemy = 1;
         public enemy1 (int enm)
@@ -12,7 +12,8 @@ public class SpawnEnemy : MonoBehaviour {
         }
     }
      public float Timer = 2;
-     public GameObject clone;
+    private int enemy;
+    public GameObject clone;
      float Enemies = 1;
      GameObject Clone;
 
@@ -27,12 +28,9 @@ public class SpawnEnemy : MonoBehaviour {
         {
         Clone = Instantiate(clone, ObjectLocation) as GameObject;
         Timer = 2f;
-        }
-        
-        
+        }  
      }
-        
-    }
+     }
 }
 //Clone = Instantiate(clone, new Vector3(Random.Range(-9, 9), 5f, 0f), transform.rotation) as GameObject;
 //used a tutorial
